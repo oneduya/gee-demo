db04dc78b0f6ca8d74b3cbe7811846a9d3c4c82a
WAHWJ <duyabingyiran@163.com>
2021-05-24 16:51:23 +0800

    day2: include router, context

---
+++ gee/gee.go
@@ -0,0 +1,43 @@
+package gee
+
+import (
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(ctx *Context)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	c := newContext(writer, request)
+	engine.router.handle(c)
+}
+
+// inner method of Engine, called by other struct methods
+func (engine *Engine) addRoute(method, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module gee-demo
+
+require (
+	gee v0.0.0
+)
+
+replace (
+	gee => ./gee
+)
+go 1.15
